2a185ebaf29f5cb58fdfb8d7a1efb86ae6cb012b
Kinda Hall <1142704468@qq.com>
2025-05-08 16:41:54 +0800

    docs: ✏️ add v0.0.2 docs

--- src/content/en/blogs/_meta.tsx
+++ src/content/en/blogs/_meta.tsx
@@ -3,6 +3,7 @@ import type { MetaRecord } from 'nextra';
 const meta: MetaRecord = {
 	background: 'Project Background',
 	'v0.0.1': 'Revornix v0.0.1',
+	'v0.0.2': 'Revornix v0.0.2',
 };
 
 export default meta;
--- src/content/zh/blogs/_meta.tsx
+++ src/content/zh/blogs/_meta.tsx
@@ -3,6 +3,7 @@ import type { MetaRecord } from 'nextra';
 const meta: MetaRecord = {
 	background: '项目背景',
 	'v0.0.1': 'Revornix v0.0.1',
+	'v0.0.2': 'Revornix v0.0.2',
 };
 
 export default meta;
