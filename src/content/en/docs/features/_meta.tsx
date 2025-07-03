b194f7fe2446354b5892990803c0c6eeaba4bea8
Kinda Hall <1142704468@qq.com>
2025-07-03 16:30:07 +0800

    docs: ✏️ 补充文档

--- src/content/en/docs/features/_meta.tsx
+++ src/content/en/docs/features/_meta.tsx
@@ -2,11 +2,13 @@ import type { MetaRecord } from 'nextra';
 
 const meta: MetaRecord = {
 	dashboard: 'Dashboard',
+	account: 'Account',
 	document: 'Document Manage',
-	engine: 'Engine Manage',
+	engine: 'Document Converter',
 	'hot-search': 'Hot Search',
 	mcp: 'Built-In MCP',
 	notification: 'Notification Manage',
+	model: 'Custom Model',
 	'revornix-ai': 'Revornix AI',
 	section: 'Section Manage',
 	api: 'API Manage'
--- src/content/zh/docs/features/_meta.tsx
+++ src/content/zh/docs/features/_meta.tsx
@@ -2,11 +2,13 @@ import type { MetaRecord } from 'nextra';
 
 const meta: MetaRecord = {
 	dashboard: '面板查看',
+	account: '账户体系',
 	document: '文档管理',
-	engine: '引擎管理',
+	engine: '文档解析',
 	'hot-search': '热搜面板',
 	mcp: '内置MCP',
 	notification: '通知管理',
+	model: '自定义模型',
 	'revornix-ai': '智能助手',
 	section: '专栏管理',
 	api: 'API管理'
