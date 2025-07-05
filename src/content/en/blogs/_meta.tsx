013743d182ff784757e4ca933cab40b656440fcb
Kinda Hall <1142704468@qq.com>
2025-07-05 18:03:45 +0800

    docs: ✏️ add docs

--- src/content/en/blogs/_meta.tsx
+++ src/content/en/blogs/_meta.tsx
@@ -1,6 +1,7 @@
 import type { MetaRecord } from 'nextra';
 
 const meta: MetaRecord = {
+	'v0.2.6': 'Revornix v0.2.6',
 	'v0.2.5': 'Revornix v0.2.5',
 	'v0.2.0': 'Revornix v0.2.0',
 	'v0.1.0': 'Revornix v0.1.0',
