9ad507ab403488983083076044c8ef161cf871e4
Kinda Hall <1142704468@qq.com>
2026-01-15 15:23:22 +0800

    feat: 🎸 Extract the blog pages into an independent structure

--- src/app/[lang]/blogs/[[...mdxPath]]/page.tsx
+++ src/app/[lang]/blogs/[[...mdxPath]]/page.tsx
@@ -3,18 +3,24 @@ import { generateStaticParamsFor, importPage } from 'nextra/pages';
 
 export const generateStaticParams = async () => {
 	const allParams = await generateStaticParamsFor('mdxPath')();
-	return allParams.filter((params) => params.mdxPath?.[0] !== 'blogs');
+	return allParams
+		.filter((params) => params.mdxPath?.[0] === 'blogs')
+		.map((params) => ({
+			...params,
+			mdxPath: params.mdxPath?.slice(1) ?? [],
+		}));
 };
 
 export async function generateMetadata(props: PageProps) {
 	const params = await props.params;
-	const { metadata } = await importPage(params.mdxPath, params.lang);
+	const mdxPath = params.mdxPath ?? [];
+	const { metadata } = await importPage(['blogs', ...mdxPath], params.lang);
 	return metadata;
 }
 
 type PageProps = Readonly<{
 	params: Promise<{
-		mdxPath: string[];
+		mdxPath?: string[];
 		lang: string;
 	}>;
 }>;
@@ -23,14 +29,15 @@ const Wrapper = useMDXComponents().wrapper;
 
 export default async function Page(props: PageProps) {
 	const params = await props.params;
-	const result = await importPage(params.mdxPath, params.lang);
+	const mdxPath = params.mdxPath ?? [];
+	const result = await importPage(['blogs', ...mdxPath], params.lang);
 
 	const { default: MDXContent, toc, metadata, sourceCode } = result;
 
 	return (
-		<div className='x:bg-nextra-bg'>
+		<div>
 			<Wrapper toc={toc} metadata={metadata} sourceCode={sourceCode}>
-				<MDXContent {...props} params={params} />
+				<MDXContent {...props} params={{ ...params, mdxPath }} />
 			</Wrapper>
 		</div>
 	);
